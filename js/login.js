f0ecacc06c7da705dcac8e2339add66a04440446
JonasDT <jonas.del.turco@gmail.com>
2021-05-23 16:11:51 +0200

    Create of login and register

---
+++ js/login.js
@@ -0,0 +1,27 @@
+const btnLogin = document.querySelector(".login button").addEventListener("click", (e) => {
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    fetch('http://localhost:3000/users/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username":username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if(json.status === "success") {
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "index.html";
+        } else {
+            let feedback = document.querySelector(".alert");
+            feedback.textContent = "Login failed buddy.";
+            feedback.classList.remove('hidden');
+        }
+    })
+});
