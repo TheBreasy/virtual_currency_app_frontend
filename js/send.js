303fc699b4f13742e1f3acd3f9691e1b68b091c7
JonasDT <jonas.del.turco@gmail.com>
2021-05-29 02:06:53 +0200

    made autofill

--- js/send.js
+++ js/send.js
@@ -4,11 +4,63 @@ if(!localStorage.getItem("token")) {
     window.location.href = "login.html";
 }
 
-//AUTO COMPLETE BEGIN
-// document.querySelector("#recipient").addEventListener("keyup", () => {
-//     let recipient = document.querySelector("#recipient").value;
-//     console.log(recipient);
-// });
+const recipient = document.querySelector('#recipient');
+const autocomplete = document.querySelector('#autocomplete');
+
+const searchName = async (searchText) => {
+    let res = await fetch(base_url + "/users/", {
+        method: "get",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const json = await res.json();
+    let users = json.users;
+
+    let matches = users.filter(user =>{
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return user.nickname.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+        document.querySelector('#autocomplete').innerHTML = "";
+    }
+
+    outputHtml(matches);
+};
+
+const outputHtml = matches => {
+        if (matches.length>0){
+            // matches.forEach(match =>{
+                const html = matches.map(match => `
+                    <div class="pt-1">
+                        <a class="color-blue autofill">${match.nickname}</a>
+                    </div>
+                `).join('');
+                autocomplete.innerHTML = html;
+
+                console.log(matches);
+                console.log(html);
+
+        //         document.querySelector('.autofill').addEventListener("click", function (e){
+        //             console.log('clicked');
+        //             e.preventDefault();
+        //             document.querySelector('#recipient').value = document.querySelector('.autofill').innerHTML;
+        //     })
+        // })
+
+            document.querySelectorAll('.autofill').forEach(item => {
+                item.addEventListener('click', event => {
+                    console.log('clicked');
+                    event.preventDefault();
+                    document.querySelector('#recipient').value = document.querySelector('.autofill').innerHTML;
+                })
+            })
+    }
+}
+
+recipient.addEventListener('input', () =>  searchName(recipient.value));
 
 document.querySelector("#send").addEventListener("click", () => {
     let recipient = document.querySelector("#recipient").value;
